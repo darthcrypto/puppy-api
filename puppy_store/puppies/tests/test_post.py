646a71ddda1368b884da96fd54271437281566c8
darthcrypto <michaelelgayar@gmail.com>
2019-08-16 13:08:38 -0400

    Add tests for post

---
+++ puppy_store/puppies/tests/test_post.py
@@ -0,0 +1,43 @@
+from django.test import TestCase, Client
+from ..models import Puppy
+from ..serializers import PuppySerializer
+import json
+from rest_framework import status
+from django.urls import reverse
+
+#initialize the APIClient app
+client = Client()
+
+class CreateNewPuppyTest(TestCase):
+    """ Test module for inserting a new puppy """
+
+    def setUp(self):
+        self.valid_payload = {
+            'name': 'Muffin',
+            'age': 4,
+            'breed': 'Pamerion',
+            'color': 'White'
+        }
+
+        self.invalid_payload = {
+            'name': '',
+            'age': 4,
+            'breed': 'Pamerion',
+            'color': 'White'
+        }
+
+    def test_create_valid_puppy(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+    
+    def test_create_invalid_puppy(self):
+        response = client.post(
+            reverse('get_post_puppies'),
+            data=json.dumps(self.invalid_payload),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
