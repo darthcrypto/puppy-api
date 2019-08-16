192d0a180edea7f211068ad34c35f65a03c6fd00
darthcrypto <michaelelgayar@gmail.com>
2019-08-16 07:22:00 -0400

    Add test case for get all puppies

---
+++ puppy_store/puppies/tests/test_get.py
@@ -0,0 +1,31 @@
+from django.test import TestCase, Client
+from ..models import Puppy
+from ..serializers import PuppySerializer
+import json
+from rest_framework import status
+from django.urls import reverse
+
+#initialize the APIClient app
+client = Client()
+
+class GetAllPuppies(TestCase):
+    """ Test module for GET all puppies API """
+    
+    def setUp(self):
+        Puppy.objects.create(
+            name='Casper', age=3, breed='Bull Dog', color='Black')
+        Puppy.objects.create(
+            name='Muffin', age=1, breed='Gradane', color='Brown')
+        Puppy.objects.create(
+            name='Rambo', age=2, breed='Labrador', color='Black')
+        Puppy.objects.create(
+            name='Ricky', age=6, breed='Labrador', color='Brown')
+        
+    def test_get_all_puppies(self):
+        #get API response
+        response = client.get(reverse('get_post_puppies'))
+        #get data from db
+        puppies = Puppy.objects.all()
+        serializer = PuppySerializer(puppies, many=True)
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
