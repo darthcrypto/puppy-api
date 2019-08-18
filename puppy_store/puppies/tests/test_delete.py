d87f99f549b6726fae70501c19e44e24bd6497d4
darthcrypto <michaelelgayar@gmail.com>
2019-08-18 07:21:34 -0400

    Add delete test

---
+++ puppy_store/puppies/tests/test_delete.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, Client
+from ..models import Puppy
+from ..serializers import PuppySerializer
+import json
+from rest_framework import status
+from django.urls import reverse
+
+#initialize the APIClient app
+client = Client()
+
+class DeleteSinglePuppy(TestCase):
+    """ Test Module for deleting an existing puppy record """
+
+    def setUp(self):
+        self.casper = Puppy.objects.create(
+            name='Casper', age=3, breed='Bull Dog', color='Black')
+        self.muffin = Puppy.objects.create(
+            name='Muffy', age=1, breed='Gradane', color='Brown')
+    
+    def test_valid_delete_puppy(self):
+        response = client.delete(
+            reverse = client.delete('get_delete_update_puppy', kwargs={'pk': self.muffin.pk}))
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_delete_puppy(self):
+        response = client.delete(
+            reverse('get_delete_update_puppy', kwargs={'pk': 30}))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+        
